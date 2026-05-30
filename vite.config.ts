c25fc27d1077d3558c5d7e03d2c384e47edd82d6
dondisalapsi-commits <dondisalapsi@gmail.com>
2026-05-30 20:11:25 +0300

    Build dosyaları yayına hazır

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/DemoSite/',
   plugins: [react()],
-  base: '/DemoSite/', // <-- Bu satırı mutlaka ekle! (Depo isminle aynı olmalı)
 })
